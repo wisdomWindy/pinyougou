d092e39682499c94a0308e5f18383d0b95d99329
wisdomWindy <1659140773@qq.com>
2021-01-27 20:19:24 +0800

    “5.0”

---
+++ pinyougou-main/components/goodslist/goodslist.js
@@ -0,0 +1,23 @@
+// components/goodslist/goodslist.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    goods:Object
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
